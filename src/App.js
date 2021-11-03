2f3a1409d0afe40a3ea7ec035ac56269438ec171
putri <putrisanjiwanidewi@gmail.com>
2021-11-03 21:18:11 +0900

    delet routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from "./Component/Navbar";
-import Home from "../src/Page/Home/Home";
 import "./App.css";
+import Layout from "./Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/home" exact>
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <Layout/>
     </div>
   );
 }
